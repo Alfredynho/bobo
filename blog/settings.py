2e3f7d080b6e90a83a7e2fa8d23f78d735c3a87c
Alfredo Callizaya <callizayagutierrezalfredo@gmail.com>
2017-07-28 11:03:09 -0400

    modificacion de archivos staticos1

--- blog/settings.py
+++ blog/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public/static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
